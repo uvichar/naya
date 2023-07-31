c5fb126943b2eaee953a3f3f4beddf70550cdbab
uvichar <utkarshv167@gmail.com>
2023-07-31 21:40:02 +0530

    try

---
+++ src/bsts/prime.java
@@ -0,0 +1,26 @@
+package bsts;
+
+public class prime {
+    public static void main(String[] args) {
+        check(15);
+    }
+
+    static void check(int n) {
+        boolean[] b=new boolean[n+1];
+        for(int i=2;i<=n;i++){
+            b[i]=true;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(b[i]==true){
+                for(int j=i*i;j<=n;j=j+i){
+                    b[j]=false;
+                }
+            }
+        }for(int i=2;i<=n;i++){
+            if(b[i]==true){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
